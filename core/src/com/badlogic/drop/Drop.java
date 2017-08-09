6dfdee2233c14d028bc14afb78e24221a672446b
Michael Kemp <mkemp@accu-time.com>
2017-08-09 11:58:33 -0400

    Render bucket

--- core/src/com/badlogic/drop/Drop.java
+++ core/src/com/badlogic/drop/Drop.java
@@ -5,13 +5,21 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
 
 public class Drop extends ApplicationAdapter {
 	private Texture dropImage;
 	private Texture bucketImage;
 	private Sound dropSound;
 	private Music rainMusic;
+
+	private OrthographicCamera camera;
+	private SpriteBatch batch;
+
+	private Rectangle bucket;
 	
 	@Override
 	public void create () {
@@ -27,12 +35,37 @@ public class Drop extends ApplicationAdapter {
 		// Play music
 		rainMusic.setLooping(true);
 		rainMusic.play();
+
+		// Create camera
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 800, 480);
+
+		// Create sprite batch
+		batch = new SpriteBatch();
+
+		// Create bucket
+		bucket = new Rectangle();
+		bucket.x = 800 / 2 - 64 / 2;
+		bucket.y = 20;
+		bucket.width = 64;
+		bucket.height = 64;
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+
+		// Clear screen
+		Gdx.gl.glClearColor(0, 0, 0.2f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		// Update camera
+		camera.update();
+
+		// Render bucket
+		batch.setProjectionMatrix(camera.combined);
+		batch.begin();
+		batch.draw(bucketImage, bucket.x, bucket.y);
+		batch.end();
 	}
 	
 	@Override
